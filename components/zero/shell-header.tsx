83c3b1c02be37b9e49b9be1cd74934c0759329ea
v0agent <it+v0agent@vercel.com>
2026-06-12 22:19:30 +0000

    feat: adjust WebkitTextStroke for shell header text elements

--- components/zero/shell-header.tsx
+++ components/zero/shell-header.tsx
@@ -65,18 +65,20 @@ export function ShellHeader() {
             <span
               aria-hidden
               className="pointer-events-none absolute inset-0 select-none [transform:scaleX(-1)]"
-              style={{ WebkitTextStroke: "1px #000", paintOrder: "stroke" }}
+              style={{ WebkitTextStroke: "1.5px #000", paintOrder: "stroke" }}
             >
               z
             </span>
             <span
               className="relative"
-              style={{ WebkitTextStroke: "1px #000", paintOrder: "stroke" }}
+              style={{ WebkitTextStroke: "1.5px #000", paintOrder: "stroke" }}
             >
               z
             </span>
           </span>
-          ero
+          <span style={{ WebkitTextStroke: "1.5px #000", paintOrder: "stroke" }}>
+            ero
+          </span>
         </span>
       </div>
     </header>
